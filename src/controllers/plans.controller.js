4d53be6a9613eec9ca45dd5e56f248c74fa9161e
Ritesh Rohan <46083169+rohanrit@users.noreply.github.com>
2025-12-28 21:27:59 +0530

    fix plan api

--- src/controllers/plans.controller.js
+++ src/controllers/plans.controller.js
@@ -2,7 +2,7 @@ import pool from '../config/db.js';
 
 export const getAllPlans = async (req, res, next) => {
     try {
-        const [rows] = await pool.query('SELECT p.plan_id, p.name, p.features, p.overview, p.status, p.`order`, p.modified_on, p.created_on, pr.pricing_id, pr.currency, pr.tenure, pr.price, pr.created_on AS pricing_created_on FROM plans p LEFT JOIN plan_pricing pr ON p.plan_id = pr.plan_id ORDER BY plan_id DESC LiMIT 100');
+        const [rows] = await pool.query('SELECT * FROM plans ORDER BY plan_id ASC');
         res.json(rows);
     } catch (err) {
         next(err);
